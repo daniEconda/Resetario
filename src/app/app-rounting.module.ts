ce53f50a12fdd07a4503d397541b1e6f349e5b6d
danielconda <estebangiraldo746@gmail.com>
2023-07-24 12:21:12 -0500

    version 1.3

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recetas/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
@@ -13,8 +12,8 @@ const appRoutes: Routes = [
       { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent },
       { path: ':id/edit', component: RecipeEditComponent },
-    ] },
-    { path: 'shopping-list', component: ShoppingListComponent },
+    ]},
+    ,
   ];
   
   @NgModule({
